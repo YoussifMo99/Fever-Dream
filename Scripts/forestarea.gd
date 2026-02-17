af7c4ead5b47626234d586fb3f61dfd247b46998
YoussifMo99 <youssifbarakat99@gmail.com>
2026-02-17 23:28:57 +0400

    passwords added

--- Scripts/forestarea.gd
+++ Scripts/forestarea.gd
@@ -2,8 +2,6 @@ extends Area3D
 
 
 
-
-
 func _on_body_entered(body):
 	print("body entered: ", body.name)
 	if body.is_in_group("player"):
---
+++ Scripts/password.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var entered_code = []
+
+func _ready():
+	get_node("VBoxContainer/ROW1/1").pressed.connect(_on_button_pressed.bind(1))
+	get_node("VBoxContainer/ROW1/2").pressed.connect(_on_button_pressed.bind(2))
+	get_node("VBoxContainer/ROW1/3").pressed.connect(_on_button_pressed.bind(3))
+	get_node("VBoxContainer/ROW2/4").pressed.connect(_on_button_pressed.bind(4))
+	get_node("VBoxContainer/ROW2/5").pressed.connect(_on_button_pressed.bind(5))
+	get_node("VBoxContainer/ROW2/6").pressed.connect(_on_button_pressed.bind(6))
+	get_node("VBoxContainer/ROW3/7").pressed.connect(_on_button_pressed.bind(7))
+	get_node("VBoxContainer/ROW3/8").pressed.connect(_on_button_pressed.bind(8))
+	get_node("VBoxContainer/ROW3/9").pressed.connect(_on_button_pressed.bind(9))
+	get_node("VBoxContainer/ROW4/0").pressed.connect(_on_button_pressed.bind(0))
+	get_node("VBoxContainer/ROW4/Enter").pressed.connect(_on_enter_pressed)
+
+func _on_button_pressed(value):
+	if entered_code.size() < 4:
+		entered_code.append(value)
+		print(entered_code)
+
+func _on_enter_pressed():
+	if entered_code == [1, 2, 3, 4]:
+		print("correct!")
+	else:
+		print("wrong!")
+	entered_code.clear()
